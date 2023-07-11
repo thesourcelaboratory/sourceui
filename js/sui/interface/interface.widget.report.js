09992a5a240ff74fc828847a05cfd870a61da27b
Marino Boscolo <marino@sourcelab.com.br>
2023-07-11 11:33:18 -0300

    050

--- js/sui/interface/interface.widget.report.js
+++ js/sui/interface/interface.widget.report.js
@@ -90,7 +90,7 @@ sourceui.interface.widget.report = function($widget,setup){
 	Report.document.addClass('preventhistorystack');
 
 	var ___cnsl = {
-		active: true,
+		active: false,
 		stack: function(where){
 			___cnsl.green('initStack',where);
 		},
